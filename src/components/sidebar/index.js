82bdeef697316b36ecef0d1b215ca54cc3e9a897
Jose Nieves-Diaz <issaidiaz2@gmail.com>
2021-12-31 20:53:26 -0500

    added fixed element to sidebar component

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,25 +1,3 @@
-// import React, { useContext } from 'react';
-// import useUser from '../../hooks/use-user';
-// import Suggestions from './suggestions';
-// import User from './user';
-
-// export default function Sidebar() {
-
-//   //------you must have useUser() or else it wont work
-//     const { 
-//         user: { docId, fullName, username, userId, following }
-//     } = useUser();
-//   //--------------
-//     return (
-//         <div className="p-4">
-//             <User username={username} fullName={fullName} />
-//             <Suggestions userId={userId} following={following} loggedInUserDocId={docId} />
-//         </div>
-//     )
-// }
-
-//-----------------------------
-
 import React, { useContext } from 'react';
 import LoggedInUserContext from '../../context/logged-in-user';
 import Suggestions from './suggestions';
@@ -28,13 +6,15 @@ import User from './user';
 export default function Sidebar() {
 
   //------you must have useUser() or else it wont work
-    const { 
-        user: { docId = '', fullName, username, userId, following } = {} } = useContext(LoggedInUserContext);
+  const { 
+    user: { docId = '', fullName, username, userId, following } = {} } = useContext(LoggedInUserContext);
   //--------------
-    return (
-        <div className="p-4">
-            <User username={username} fullName={fullName} />
-            <Suggestions userId={userId} following={following} loggedInUserDocId={docId} />
-        </div>
-    )
+  return (
+    <div className="relative p-4">
+      <div className="fixed">
+        <User username={username} fullName={fullName} />
+        <Suggestions userId={userId} following={following} loggedInUserDocId={docId} />
+      </div>
+    </div>
+  )
 }
